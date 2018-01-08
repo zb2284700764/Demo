8c57e46a53701c6c5d61a2def887757c11d15324
zhoubin <aa@163.com>
2018-01-08 14:19:44 +0800

    两种反射方法之间增加打印间隔

--- demo/src/test/java/com/demo/reflect/ReflectTest.java
+++ demo/src/test/java/com/demo/reflect/ReflectTest.java
@@ -21,11 +21,13 @@ public class ReflectTest {
 		 * getFields 获取的是为public的属性，包括父类中定义，
 		 * getDeclaredFields 获取的是指定类定义的所有定义的属性，不包括父类的。
 		 */
+		System.out.println("getDeclaredFields 方法获取的属性");
 		Field[] fieldArray1 = c.getDeclaredFields();
 		for (Field field : fieldArray1) {
 			System.out.println(field);
 		}
 		System.out.println("*************************************************************************************");
+		System.out.println("getFields 方法获取的属性");
 		Field[] fieldArray2 = c.getFields();
 		for (Field field : fieldArray2) {
 			System.out.println(field);
@@ -34,27 +36,30 @@ public class ReflectTest {
 		System.out.println("\n-------------------------------------------------------------------------------------\n");
 		
 		// 获取 方法
+		System.out.println("getDeclaredMethods 方法获取的方法");
 		Method[] m1 = c.getDeclaredMethods();
 		for (Method method : m1) {
 			System.out.println(method);
 		}
 		System.out.println("*************************************************************************************");
+		System.out.println("getMethods 方法呼叫哦去的方法");
 		Method[] m2 = c.getMethods();
 		for (Method method : m2) {
 			System.out.println(method);
 		}
 
 		System.out.println("\n-------------------------------------------------------------------------------------\n");
-		
-		// 使用getConstructors获取构造器
-		Constructor<?>[] constructors = c.getConstructors();
-		for (Constructor<?> m : constructors) {
+
+		// 获取构造器
+		System.out.println("getDeclaredConstructors 方法获取的构造方法");
+		Constructor<?>[] constructors1 = c.getDeclaredConstructors();
+		for (Constructor<?> m : constructors1) {
 			System.out.println(m);
 		}
 		System.out.println("*************************************************************************************");
-		// 使用getDeclaredConstructors获取构造器
-		constructors = c.getDeclaredConstructors();
-		for (Constructor<?> m : constructors) {
+		System.out.println("getConstructors 方法获取的构造方法");
+		Constructor<?>[] constructors2 = c.getConstructors();
+		for (Constructor<?> m : constructors2) {
 			System.out.println(m);
 		}
 
