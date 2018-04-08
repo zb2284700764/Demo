ee45d7116e6f5da05a86677cd3a4cc22d9fc9c27
zhoubin <aa@163.com>
2018-04-08 14:05:21 +0800

    完成 String,List,Hash,Set,ZSet 基本命令demo

--- demo/src/test/java/com/demo/redis/TestRedis.java
+++ demo/src/test/java/com/demo/redis/TestRedis.java
@@ -37,36 +37,71 @@ public class TestRedis {
 
         // 得到旧的 value 并设置新的 value
         System.out.println(jedis.getSet(key, "张三，你好"));
-        System.out.println(jedis.get(key));
+        System.out.println("得到旧的值并将新的值set到对应的key：" + jedis.get(key));
 
+        // 截取 value 中的字符串，一个汉字对应 3 个下标，一个中文标点符号占 3 个下标，下标从 0 开始
+        jedis.set("hello", "张三，你好");
+        System.out.println("截取 value 中的字符串，一个汉字对应 3 个下标：" + jedis.getrange("hello", 0, 11));
+        jedis.set("hello1", "张三你好");
+        System.out.println("截取 value 中的字符串，一个汉字对应 3 个下标：" + jedis.getrange("hello1", 0, 8));
+        jedis.set("hello2", "张三,你好");
+        System.out.println("截取 value 中的字符串，一个汉字对应 3 个下标：" + jedis.getrange("hello2", 0, 9));
+
+
+        // 设置 key 的过期时间 秒
+        jedis.setex("key", 2, "这个key会在2秒之后消失");
+        System.out.println("通过 setex 设置 key 的过期时间单位（秒）：" + jedis.get("key"));
+
+        // 当 name1 对应的 key 不存在的时候才会设置值
+        jedis.setnx("name1", "name1");
+        System.out.println("当 name1 对应的 key 不存在的时候才会设置值" + jedis.get("name1"));
+
+        // 用 value 参数覆写给定 key 所储存的字符串值，从偏移量 offset 开始。
+        jedis.setrange("name1", 4, "把数字1替换了");
+        System.out.println("从指定偏移量开始替换字符串中的值：" + jedis.get("name1"));
+
+        // key 中对应字符串的长度
+        System.out.println("获取 key 对应的字符串的长度：" + jedis.strlen(key));
 
         // 设置多个键值对
         jedis.mset("key1", "value1", "key2", "value2", "key3", "value3");
         // 获取多个键对应的值
         List<String> list = jedis.mget("key1", "key2");
-        System.out.println(list);
+        System.out.println("得到多个key的value，返回的是 List：" + list);
 
-        // key 中对应字符串的长度
-        System.out.println(jedis.strlen(key));
+        // 同时设置一个或多个 key-value 对，当且仅当所有给定 key 都不存在。
+        System.out.println("同时设置一个或多个 key-value 对，当且仅当所有给定 key 都不存在：" + jedis.msetnx("key1", "value1", "key4", "value4"));
+        System.out.println("获取所有的 key ：" + jedis.mget("key1", "key2", "key3", "key4"));
 
-        // 截取 value 中的字符串，一个汉字对应 3 个下标
-        int startOffset = 0;
-        int endOffset = 11;
-        System.out.println(jedis.getrange(key, startOffset, endOffset));
-
-        // 设置 key 的过期时间 秒
-        jedis.setex("key", 2, "这个key会在2秒之后消失");
-        System.out.println(jedis.get("key"));
+        System.out.println("同时设置一个或多个 key-value 对，当且仅当所有给定 key 都不存在：" + jedis.msetnx("key5", "value5", "key4", "value4"));
+        System.out.println("获取所有的 key ：" + jedis.mget("key1", "key2", "key3", "key4", "key5"));
 
         // 设置 key 的过期时间 毫秒
-        jedis.psetex("key11", 1000, "这个value会在1000毫秒之后消失");
-        System.out.println(jedis.get("key11"));
+        jedis.psetex("key11", 1000L, "这个value会在1000毫秒之后消失");
+        System.out.println("通过 psetex 设置 key 的过期时间，单位（毫秒）：" + jedis.get("key11"));
+
+        // 将 key 中储存的数字值增一
+        jedis.set("int1", "1");
+        System.out.println("将 key 中储存的数字值增一：" + jedis.incr("int1"));
+
+        // 将 key 所储存的值加上给定的增量值（increment）
+        System.out.println("将 key 所储存的值加上给定的增量值（increment）：" + jedis.incrBy("int1", 5));
+        ;
+        // 将 key 所储存的值加上给定的浮点增量值（increment）
+        System.out.println("将 key 所储存的值加上给定的浮点增量值（increment）：" + jedis.incrByFloat("int1", 5.6));
+
+        // 将 key 中储存的数字值减一，减的时候不能是 float 类型
+        jedis.set("int2", "6");
+        System.out.println("将 key 中储存的数字值减一：" + jedis.decr("int2"));
+
+        // key 所储存的值减去给定的减量值（decrement）
+        System.out.println("key 所储存的值减去给定的减量值（decrement）：" + jedis.decrBy("int2", 3));
 
         // 删除指定 key 的数据
         jedis.del(key);
-        System.out.println(jedis.get(key));
+        System.out.println("删除对应的 key ：" + jedis.get(key));
 
-        // 删除所有的 key
+        // 清空整个 redis 缓存中的数据
         jedis.flushDB();
     }
 
@@ -82,38 +117,59 @@ public class TestRedis {
         map.put("name", "yc");
         map.put("age", "22");
         map.put("qq", "1933108196");
+        map.put("score", "88");
         // 通过 hmset 命令向 redis 中增加 map 类型的数据
         jedis.hmset("user", map);
+
         // 通过 hset 将新的值 set 到 map 中对应字段
         jedis.hset("user", "name", "张三");
-        // 获取 map 中所有的 key-value 集合
-        System.out.println(jedis.hgetAll("user"));
+
+        // 只有在字段 field 不存在时，设置哈希表字段的值
+        jedis.hsetnx("user", "name", "哈哈");
+        System.out.println("只有在字段 field 不存在时，设置哈希表字段的值：" + jedis.hget("user", "name"));
+
+        // 查看哈希表 key 中，指定的字段是否存在
+        System.out.println("查看哈希表 key 中，指定的字段是否存在：" + jedis.hexists("user", "age"));
+
         // 通过 hmget 命令获取对应 map 中的一个或多个 key 对应的 value
-        System.out.println(jedis.hmget("user", "name", "age"));
+        System.out.println("获取对应 map 中的一个或多个 key 对应的 value ：" + jedis.hmget("user", "name", "age"));
+
+        // 获取 map 中所有的 key-value 集合
+        System.out.println("获取 map 中所有的 key-value 集合：" + jedis.hgetAll("user"));
+
+        // 为哈希表 key 中的指定字段的整数值加上增量 increment
+        jedis.hincrBy("user", "age", 3);
+        System.out.println("为哈希表 key 中的指定字段的整数值加上增量 increment ：" + jedis.hget("user", "age"));
+
+        // 为哈希表 key 中的指定字段的浮点数值加上增量 increment
+        jedis.hincrByFloat("user", "score", 4.5);
+        System.out.println("为哈希表 key 中的指定字段的浮点数值加上增量 increment ：" + jedis.hget("user", "score"));
 
         // 直接通过 key - field - value 的形式向 Hash 中存储数据
         jedis.hset("map", "name", "李四");
         // 获取 map 中指定字段的值
-        System.out.println(jedis.hget("map", "name"));
+        System.out.println("获取 map 中 key 指定字段的 value ：" + jedis.hget("map", "name"));
+
         // 获取 map 中所有的 key-value 集合
-        System.out.println(jedis.hgetAll("map"));
+        System.out.println("获取 map 中所有的 key-value ：" + jedis.hgetAll("map"));
 
         // 返回 key 为 user 的键中 value 的个数
-        System.out.println(jedis.hlen("user"));
+        System.out.println("获取 key 为 user 的键中 value 的个数：" + jedis.hlen("user"));
 
         // 是否存在 key 为 user 的记录，返回 Boolean
-        System.out.println(jedis.exists("user"));
+        System.out.println("检测是否存在 key 为 user 的记录：" + jedis.exists("user"));
 
         // 获取 key 为 user 的 map 中的所有 key 集合
-        System.out.println(jedis.hkeys("user"));
+        System.out.println("获取 key 为 user 的 Hash 中的所有 key 集合：" + jedis.hkeys("user"));
 
         // 获取 key 为 user 的 map 中的所有 value 集合
-        System.out.println(jedis.hvals("user"));
+        System.out.println("获取 key 为 user 的 Hash 中的所有 value 集合：" + jedis.hvals("user"));
 
         // 删除 hash 表中的一个或多个字段
         jedis.hdel("user", "name");
+        System.out.println("删除 key 为 user 的 Hash 中的 key 为 name 的字段：" + jedis.hmget("user", "name"));
 
-        // 清空数据
+        // 清空整个 redis 缓存中的数据
         jedis.flushDB();
     }
 
@@ -125,50 +181,85 @@ public class TestRedis {
         Jedis jedis = JedisUtil.getJedis();
         jedis.select(15); // 切换为第16个数据库
 
-        // 从左到右依次将数据插入列表头部 正好和 rpush 插入顺序相反
+        // 从左到右依次将 "张三", "张三", "李四", "王五" 插入列表头部 正好和 rpush 插入顺序相反
         jedis.lpush("list", "张三", "张三", "李四", "王五");
+        System.out.println("从左到右依次将 \"张三\", \"张三\", \"李四\", \"王五\" 插入列表头部 正好和 rpush 插入顺序相反：" + jedis.lrange("list", 0, -1));
 
         // 在元素“张三”之前插入一个元素“哈哈”
         jedis.linsert("list", BinaryClient.LIST_POSITION.BEFORE, "张三", "哈哈");
+        System.out.println("在元素“张三”之前插入一个元素“哈哈”：" + jedis.lrange("list", 0, -1));
+
         // 在元素“王五”之后插入“哈哈2”
         jedis.linsert("list", BinaryClient.LIST_POSITION.AFTER, "王五", "哈哈2");
+        System.out.println("在元素“王五”之后插入“哈哈2”：" + jedis.lrange("list", 0, -1));
 
         // 获取 list 中所有的数据，value 允许重复
-        System.out.println(jedis.lrange("list", 0, -1));
+        System.out.println("获取 list 中所有的数据，value 允许重复：" + jedis.lrange("list", 0, -1));
 
         // 获取 list 中指定范围的数据
-        System.out.println(jedis.lrange("list", 0, 1));
+        System.out.println("获取 list 中指定范围 0 - > 1 之间的数据" + jedis.lrange("list", 0, 1));
 
         // 通过下标获取指定位置的数据
-        System.out.println(jedis.lindex("list", 2));
+        System.out.println("通过索引获取列表中的元素：" + jedis.lindex("list", 2));
 
+        // 获取列表长度
+        System.out.println("获取列表长度：" + jedis.llen("list"));
+
+        // 移出并获取列表的第一个元素，并得到删除的值，如果没有值会返回 null
+        jedis.lpop("list");
+        System.out.println("移出并获取列表的第一个元素，并得到删除的值，如果没有值会返回 null：" + jedis.lrange("list", 0, -1));
+
+        // 将一个或多个值插入到已存在的列表头部
+        jedis.lpushx("list", "王五");
+        System.out.println("将一个或多个值插入到已存在的列表头部：" + jedis.lrange("list", 0, -1));
+
+        // 使用 lrem 移除列表中的元素
+        jedis.lpush("testList", "9", "8", "7", "8", "6", "8", "8", "8", "8", "8", "5", "4", "8", "3", "8", "2", "1");
+        System.out.println("testList 列表中的所有元素：" + jedis.lrange("testList", 0, -1));
+        jedis.lrem("testList", 3, "8");
+        System.out.println("移除列表中的元素，count > 0：从头往尾移除值为 value 的元素，移除的数量为 count 个：" + jedis.lrange("testList", 0, -1));
+        ;
+        jedis.lrem("testList", -5, "8");
+        System.out.println("移除列表中的元素，count < 0：从尾往头移除值为 value 的元素，移除的数量为 count 个：" + jedis.lrange("testList", 0, -1));
+        ;
+        jedis.lrem("testList", 0, "8");
+        System.out.println("移除列表中的元素，count = 0：表示移除所有值为 value 的元素：" + jedis.lrange("testList", 0, -1));
+
+        // 通过索引设置列表元素的值，会把对应位置的值给覆盖掉
+        jedis.lset("testList", 0, "0");
+        System.out.println("通过索引设置列表元素的值，会把对应位置的值给覆盖掉：" + jedis.lrange("testList", 0, -1));
+
+        // 对一个列表进行修剪(trim)，就是说，让列表只保留指定区间内的元素，不在指定区间之内的元素都将被删除
+        jedis.ltrim("testList", 0, 5);
+        System.out.println("对一个列表进行修剪(trim)只保留指定区间内的元素：" + jedis.lrange("testList", 0, -1));
+
+        // 移除并获取列表最后一个元素，并得到删除的值，如果没有值会返回 null
+        System.out.println("移除并获取列表最后一个元素，并得到删除的值，如果没有值会返回 null：" + jedis.rpop("testList"));
+
+        // 移除列表的最后一个元素，并将该元素添加到另一个列表并返回
+        jedis.rpoplpush("list", "newList");
+        System.out.println("移除列表的最后一个元素，并将该元素添加到另一个列表并返回：" + jedis.lrange("newList", 0, -1));
+
+        // 删除对应 key 的数据
         jedis.del("list");
 
         // 从左向右依次将数据插入列表尾部  正好和 lpush 插入顺序相反
         jedis.rpush("list", "张三", "李四", "王五");
-        System.out.println(jedis.lrange("list", 0, 1));
-        System.out.println(jedis.lindex("list", 2));
+        System.out.println("从左向右依次将数据插入列表尾部  正好和 lpush 插入顺序相反：" + jedis.lrange("list", 0, 1));
 
+        // 通过索引获取列表中的元素
+        System.out.println("通过索引获取列表中的元素：" + jedis.lindex("list", 2));
 
         // 从队列（list）的右边入队一个元素，仅队列存在时有效。当队列不存在时，不进行任何操作
         long index = jedis.rpushx("list", "赵六");
-        System.out.println(index);
-        System.out.println(jedis.lrange("list", 0, index));
+        System.out.println("从队列（list）的右边入队一个元素，仅队列存在时有效。当队列不存在时，不进行任何操作" + jedis.lrange("list", 0, index));
 
-        // 从队列（list）的头部删除一个元素
-        System.out.println(jedis.lpop("list"));
-        System.out.println(jedis.lpop("list"));
-
-        // 从队列（list）的尾部删除一个元素
-        System.out.println(jedis.rpop("list"));
-        System.out.println(jedis.rpop("list"));
-        System.out.println(jedis.rpop("list"));
-
+        // 为已存在的列表添加值，并返回对应的下标，如果不存在对应的列表将增加不成功
         long in = jedis.rpushx("list", "张三十年");
-        System.out.println(in);
-        System.out.println(jedis.lrange("list", 0, in));
+        System.out.println("为已存在的列表添加值，并返回对应的下标，如果不存在对应的列表将增加不成功："+in);
+        System.out.println("为已存在的列表添加值，并返回对应的下标，如果不存在对应的列表将增加不成功："+jedis.lrange("list", 0, in));
 
-        // 清空数据
+        // 清空整个 redis 缓存中的数据
         jedis.flushDB();
     }
 
@@ -185,65 +276,70 @@ public class TestRedis {
         jedis.sadd("name3", "赵六", "王五", "test3");
 
         // 获取集合中的成员数
-        System.out.println(jedis.scard("name1"));
+        System.out.println("获取集合中的成员个数："+jedis.scard("name1"));
 
         // 获取集合中的所有成员
-        System.out.println(jedis.smembers("name2"));
+        System.out.println("获取集合中的所有成员："+jedis.smembers("name2"));
+
+        // 判断 member 元素是否是集合 key 的成员
+        System.out.println("判断 member 元素是否是集合 key 的成员："+jedis.sismember("name2", "张三"));
 
         // 返回 name1 集合和 name1,name3 集合的差集
-        System.out.println(jedis.sdiff("name1", "name2", "name3"));
+        System.out.println("返回 name1 集合和 name1,name3 集合的差集："+jedis.sdiff("name1", "name2", "name3"));
         // 返回 name1 集合和 name1,name3 集合的差集
-        System.out.println(jedis.sdiff("name2", "name1", "name3"));
+        System.out.println("返回 name1 集合和 name1,name3 集合的差集："+jedis.sdiff("name2", "name1", "name3"));
         // 返回 name1 集合和 name1,name3 集合的差集
-        System.out.println(jedis.sdiff("name3", "name2", "name1"));
+        System.out.println("返回 name1 集合和 name1,name3 集合的差集："+jedis.sdiff("name3", "name2", "name1"));
 
         // 将 name1 和 name2 的差集存储在 destination 集合中
         jedis.sdiffstore("destination_diff", "name1", "name2");
-        System.out.println(jedis.smembers("destination_diff"));
+        System.out.println("将 name1 和 name2 的差集存储在 destination 集合中："+jedis.smembers("destination_diff"));
 
         // 获取传入参数的所有集合的交集
-        System.out.println(jedis.sinter("name1", "name2"));
+        System.out.println("获取传入参数的所有集合的交集："+jedis.sinter("name1", "name2"));
         // 没有交集返回空
-        System.out.println(jedis.sinter("name1", "name2", "name3"));
+        System.out.println("没有交集返回空："+jedis.sinter("name1", "name2", "name3"));
 
         // 获得集合的交集并将结果存储在另外一个集合中
         jedis.sinterstore("destination_inter", "name1", "name2");
-        System.out.println(jedis.smembers("destination_inter"));
+        System.out.println("获得集合的交集并将结果存储在另外一个集合 destination_inter 中："+jedis.smembers("destination_inter"));
 
         // 将集合中的元素从 name1 集合移动到 destination_move 集合
         jedis.smove("name1", "destination_move", "李四");
-        System.out.println(jedis.smembers("destination_move"));
+        System.out.println("将集合中的元素从 name1 集合移动到 destination_move 集合："+jedis.smembers("destination_move"));
 
         // 随机移除集合中的一个元素并返回这个值
-        System.out.println(jedis.spop("name1"));
-        System.out.println(jedis.smembers("name1"));
+        System.out.println("随机移除集合中的一个元素并返回这个值："+jedis.spop("name1"));
+        System.out.println("获取结婚中的所有成员：："+jedis.smembers("name1"));
 
         // 随机返回集合中的一个或多个元素
-        System.out.println(jedis.srandmember("name2", 2));
+        System.out.println("随机返回集合中的一个或多个元素："+jedis.srandmember("name2", 2));
 
         // 移除集合中的一个或多个元素
         jedis.srem("name2", "张三");
-        System.out.println(jedis.smembers("name2"));
+        System.out.println("移除集合中的一个或多个元素：："+jedis.smembers("name2"));
 
         // 返回所有集合的并集
-        System.out.println(jedis.sunion("name1", "name2", "name3"));
+        System.out.println("返回所有集合的并集："+jedis.sunion("name1", "name2", "name3"));
 
-        // 迭代集合中的元素
+        // 所有给定集合的并集存储在 destination 集合中
+        jedis.sunionstore("destination_union", "name1", "name2", "name3");
+        System.out.println("所有给定集合的并集存储在 destination 集合中：" + jedis.smembers("destination_union"));
+
+        // 迭代集合中的元素,0是开始的下标
         ScanResult<String> result = jedis.sscan("name3", "0");
-        System.out.println("sscan迭代集合：");
+        System.out.println("sscan 迭代集合 ：");
         for (String str : result.getResult()) {
             System.out.print(str + ",");
         }
 
-
-        // 清空数据
+        // 清空整个 redis 缓存中的数据
         jedis.flushDB();
     }
 
     /**
      * ZSet 有序集合 类型数据操作测试
      */
-    @Test
     public void testZSet() {
         Jedis jedis = JedisUtil.getJedis();
         jedis.select(15);
@@ -348,11 +444,9 @@ public class TestRedis {
         jedis.zadd("name7", name7Map);
         System.out.println("返回有序集合中指定区间内的成员，通过索引倒序算，分数从高到低：" + jedis.zrevrange("name7", 1, 4));
         ;
-
         // 返回有序集中指定分数区间内的成员，分数从高到低排序
         System.out.println("返回有序集中指定分数区间内的成员，分数从高到低排序：" + jedis.zrevrangeByScore("name7", 5, 1));
         ;
-
         // 返回有序集合中指定成员的排名
         System.out.println("返回有序集合中指定成员的排名：" + jedis.zrevrank("name7", "b"));
 
@@ -364,14 +458,14 @@ public class TestRedis {
         System.out.println(jedis.zrange("nameUnion", 0, -1));
 
         // zscan 迭代遍历
-        System.out.println("zscan 迭代遍历");
+        System.out.println("zscan 迭代遍历：");
         ScanResult<Tuple> result = jedis.zscan("nameUnion", "0");
         List<Tuple> list = result.getResult();
         for (Tuple tuple : list) {
             System.out.println(tuple.getScore() + " -> " + tuple.getElement());
         }
 
-        // 清空数据
+        // 清空整个 redis 缓存中的数据
         jedis.flushDB();
     }
 
